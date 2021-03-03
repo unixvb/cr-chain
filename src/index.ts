442cfa2c973c62f2436dcc0598c5f89a59cf0f58
Andrii Ivaniv <unixvb@gmail.com>
2021-03-03 13:25:54 +0200

    express setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
-import {Block} from "./entity/block";
+import express from "express";
+import bodyParser from "body-parser";
 
-const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
+import {Blockchain} from "./blockchain";
 
-console.log(fooBlock.toString());
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
+
+const app = express();
+const blockchain = new Blockchain();
+
+app.use(bodyParser.json());
+
+app.get('/blocks', (req, res) => {
+    res.json(blockchain.chain);
+});
+
+app.post('/mine', (req, res) => {
+    const block = blockchain.addBlock(req.body.data)
+
+    console.log(`New block added: ${block.toString()}`);
+
+    res.redirect('/blocks');
+});
+
+app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
